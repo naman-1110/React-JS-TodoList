099443e91c84357809db46411d29d3405bdbbfd0
naman-1110 <90150478+naman-1110@users.noreply.github.com>
2023-01-14 19:16:39 -0330

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+import ToDoList from './TodoList';
+
+function App() {
+  
+  return (
+    <div className="app">
+      <ToDoList/>
+
+    </div>
+  )
+}
+
+export default App;
